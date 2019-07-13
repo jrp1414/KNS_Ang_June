2c2aa80555ec474bc6bf3254bce2db1de339d024
Ranjan <jyotiranjan.p14@gmail.com>
2019-07-13 10:48:11 +0530

    Barrels Example 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from "@angular/forms";
 
-import {
-    HeaderComponent,
-    FooterComponent,
-    StringInterpolationComponent,
-    PropertyBindingComponent,
-    EventBindingComponent,
-    NgifComponent,
-    StructDirectivesComponent,
-    ProductListComponent,
-    ProductThumbnailComponent
-  } from "./components/components.index";
+import * as c from "./components/components.index";
 import {ShortenPipe,ConvertToPipe,FilterProductPipe} from "./pipes/pipes.index";  
 import {BasicHighlightDirective,BetterHighlightDirective,UnlessDirective} from "./directives/directives.index";
 import { AppComponent } from './app.component';
@@ -21,15 +11,15 @@ import { LoggingService } from './services/logging.service';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    StringInterpolationComponent,
-    PropertyBindingComponent,
-    EventBindingComponent,
-    NgifComponent,
-    StructDirectivesComponent,
-    ProductListComponent,
-    ProductThumbnailComponent,
+    c.HeaderComponent,
+    c.FooterComponent,
+    c.StringInterpolationComponent,
+    c.PropertyBindingComponent,
+    c.EventBindingComponent,
+    c.NgifComponent,
+    c.StructDirectivesComponent,
+    c.ProductListComponent,
+    c.ProductThumbnailComponent,
     ShortenPipe,
     ConvertToPipe,
     FilterProductPipe,
