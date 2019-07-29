a3f6ce5bdb44c2997425aa98fbfc784c76d1dcb8
Ranjan <jyotiranjan.p14@gmail.com>
2019-07-29 09:29:12 +0530

    Observables Examples completed

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Observable, Observer, Subscription } from "rxjs";
+import { Component, OnInit, OnDestroy, EventEmitter } from '@angular/core';
+import { Observable, Observer, Subscription, Subject } from "rxjs";
 import "rxjs/Rx";
 
 @Component({
@@ -7,12 +7,17 @@ import "rxjs/Rx";
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit,OnDestroy {
-  
+export class HomeComponent implements OnInit, OnDestroy {
+
 
   constructor() { }
 
-  numberSubs:Subscription;
+  numberSubs: Subscription;
+
+  subjectExample: Subject<string> = new Subject<string>();
+  subjectSubscription: Subscription;
+
+  eeExample: EventEmitter<string> = new EventEmitter<string>();
 
   ngOnInit() {
 
@@ -34,11 +39,11 @@ export class HomeComponent implements OnInit,OnDestroy {
     // //   observer.error("Some Error Occurred");
     // // }, 4000); 
 
-    
+
     // setTimeout(() => {
     //   observer.next("Third Data Released");
     // }, 6000); 
-    
+
     // setTimeout(() => {
     //   observer.complete();
     // }, 8000);
@@ -57,19 +62,71 @@ export class HomeComponent implements OnInit,OnDestroy {
 
 
 
-    let numberSubs = Observable.interval(1000).map((num)=>num);
-    let counter=1;
-    this.numberSubs =  numberSubs.subscribe((data)=>{
-      console.log(data);
-      counter++;
-      if(counter>10){
-        this.numberSubs.unsubscribe();
-      }
-    });
+    // let numberSubs = Observable.interval(1000).map((num)=>num);
+    // let counter=1;
+    // this.numberSubs =  numberSubs.subscribe((data)=>{
+    //   console.log(data);
+    //   counter++;
+    //   if(counter>10){
+    //     this.numberSubs.unsubscribe();
+    //   }
+    // });
+
+
+
+    // setTimeout(() => {
+    //   this.subjectExample.next("Released first Data load using Subject");
+    // }, 2000);
+
+    // setTimeout(() => {
+    //   this.subjectExample.next("Released Second Data load using Subject");
+    // }, 4000);
+
+    // setTimeout(() => {
+    //   this.subjectExample.complete();
+    // }, 5000);
+
+    // setTimeout(() => {
+    //   this.subjectExample.next("Released Third Data load using Subject");
+    // }, 6000);
+
+    // this.subjectSubscription= this.subjectExample.subscribe(
+    //   (data) => { console.log(data); },
+    //   (error) => { console.log(error); },
+    //   () => { console.log("Completed"); }
+    // );
+
+
+
+    setTimeout(() => {
+      this.eeExample.emit("First Data from Event Emitter");
+    }, 2000);
+
+    setTimeout(() => {
+      this.eeExample.emit("Second Data from Event Emitter");
+    }, 4000);
+
+    setTimeout(() => {
+      this.eeExample.complete();
+    }, 5000);
+
+    setTimeout(() => {
+      this.eeExample.emit("Third Data from Event Emitter");
+    }, 6000);
+
+
+    this.eeExample.subscribe(
+      (data) => { console.log(data); },
+      (error) => { console.log(error); },
+      () => { console.log("Completed"); }
+    );
+
+
   }
 
   ngOnDestroy(): void {
-    this.numberSubs.unsubscribe();
+    //this.numberSubs.unsubscribe();
+    this.subjectSubscription.unsubscribe();
   }
 
 }
