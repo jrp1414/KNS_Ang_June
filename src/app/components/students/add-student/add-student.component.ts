073bac419f38da228cb05f1f832820e92c3fe914
Ranjan <jyotiranjan.p14@gmail.com>
2019-08-13 09:17:25 +0530

    Reactive Form Basic Validators added

--- src/app/components/students/add-student/add-student.component.ts
+++ src/app/components/students/add-student/add-student.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-student',
@@ -7,28 +7,60 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./add-student.component.css']
 })
 export class AddStudentComponent implements OnInit {
-  studentForm:FormGroup;
+  studentForm: FormGroup;
+  hobbies:FormArray= new FormArray([new FormControl()]);
+  addresses:FormArray = new FormArray([
+    new FormGroup({
+      AddLine1: new FormControl(""),
+      AddLine2: new FormControl(""),
+      AddLine3: new FormControl(""),
+      City: new FormControl(""),
+      State: new FormControl("")
+    })
+  ]);
   constructor() { }
 
   ngOnInit() {
-    this.studentForm= new FormGroup({
-      FirstName:new FormControl(""),
-      LastName:new FormControl(""),
-      NotificationType:new FormControl("email"),
-      MobileNo:new FormControl(""),
-      EmailId:new FormControl(""),
-      AddLine1:new FormControl(""),
-      AddLine2:new FormControl(""),
-      AddLine3:new FormControl(""),
-      City:new FormControl(""),
-      State:new FormControl(""),
-      TermsAndConditions:new FormControl(),
-      Subjects:new FormControl()
+    this.studentForm = new FormGroup({
+      FirstName: new FormControl("",Validators.required),
+      LastName: new FormControl("",Validators.required),
+      NotificationType: new FormControl("email"),
+      MobileNo: new FormControl(""),
+      EmailId: new FormControl("",[Validators.required,Validators.email]),
+      Addresses: this.addresses,
+      TermsAndConditions: new FormControl(),
+      Subjects: new FormControl(),
+      hobbies:this.hobbies,
+      addresses:this.addresses
     });
   }
 
-  OnStudentAdded(){
+  OnStudentAdded() {
     console.log(this.studentForm.value);
+  }
+
+  AddNewHobby(){
+    this.hobbies.push(new FormControl(""));
+  }
+
+  OnNotificationChange(notificationType:string){
+    let mobileNo = this.studentForm.get("MobileNo");
+    if(notificationType==='mobile'){      
+      mobileNo.setValidators(Validators.required);
+    }else{
+      mobileNo.clearValidators();
+    }
+    mobileNo.updateValueAndValidity(); 
+  }
+
+  AddNewAddress(){
+    this.addresses.push(new FormGroup({
+      AddLine1: new FormControl(""),
+      AddLine2: new FormControl(""),
+      AddLine3: new FormControl(""),
+      City: new FormControl(""),
+      State: new FormControl("")
+    }));
   }
 
 }
